93ae09309a940f2153f6ce64f58cf68e6f854f2c
Tatyana404 <kot.kate2020@gmail.com>
2021-01-16 19:15:59 +0200

    Add fixed

--- inheritance.js
+++ inheritance.js
@@ -10,15 +10,10 @@
  *
  * @param {string} name
  * @param {string} surname
- * @param {number} year
- * @param {number} thisYear
- * @param {number} yearAdmission
+ * @param {number} entryYear
  */
 class User {
   constructor(name, surname) {
-    // if (typeof name || typeof surname !== "string") {
-    //   throw new TypeError("Parameters have unappropriate type");
-    // }
     this._name = name;
     this._surname = surname;
   }
@@ -28,9 +23,6 @@ class User {
 }
 class Student extends User {
   constructor(name, surname, entryYear) {
-    // if (typeof entryYear !== "number") {
-    //   throw new TypeError("Parameters have unappropriate type");
-    // }
     super(name, surname);
     this._entryYear = entryYear;
   }
@@ -38,9 +30,17 @@ class Student extends User {
     const currentYear = new Date();
     const course =
       currentYear.getFullYear() - this._entryYear.getFullYear() + 1;
-    if (currentYear >= this._entryYear) {
+    if (course < 1) {
       throw RangeError("You are not a student yet");
+    }
+    if (course > 5) {
+      throw RangeError("You have completed your training");
     }
     return course;
   }
 }
+
+// const courseStudent = new Student ("Petr", "Petrovich", new Date("2020")); // console.log (2)
+// const courseStudent = new Student ("Petr", "Petrovich", new Date("2017")); // console.log (5)
+// const courseStudent = new Student ("Petr", "Petrovich", new Date("2015")); // console.log (You have completed your training)
+// const courseStudent = new Student("Petr", "Petrovich", new Date("2024")); // console.log (You are not a student yet)
